98bb9fbbbc6efd0c3bc565dc960a3bdceace219c
s4698486-Ethan <141535269+s4698486-Ethan@users.noreply.github.com>
2024-05-02 15:03:39 +1000

    Add files via upload
    This code WILL be able to drive our turtlebot and outlines some basic functionality with gestures and stuff

---
+++ ROSOutlineCode.py
@@ -0,0 +1,120 @@
+'''
+Copyright (c) 2015, Mark Silliman
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+
+# A very basic TurtleBot script that moves TurtleBot forward indefinitely. Press CTRL + C to stop.  To run:
+# On TurtleBot:
+# roslaunch turtlebot_bringup minimal.launch
+# On work station:
+# python goforward.py
+
+import rospy
+from geometry_msgs.msg import Twist
+import maho.client
+
+gesture = 0
+left_distance = 0
+right_distance = 0
+move_cmd = Twist()
+max_distance = 4
+scalar = 0.1
+
+class GoForward():
+    def __init__(self):
+
+        # initiliaze
+        rospy.init_node('GoForward', anonymous=False)
+
+	    # tell user how to stop TurtleBot
+	    rospy.loginfo("To stop TurtleBot CTRL + C")
+
+        # What function to call when you ctrl + c    
+        rospy.on_shutdown(self.shutdown)
+        
+	    # Create a publisher which can "talk" to TurtleBot and tell it to move
+        # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
+        self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+     
+	    #TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
+        r = rospy.Rate(10)
+
+        # Twist is a datatype for velocity
+        #move_cmd = Twist()
+	    # let's go forward at 0.2 m/s
+        move_cmd.linear.x = 0
+	    # let's turn at 0 radians/s
+	    move_cmd.angular.z = 0
+
+	    # as long as you haven't ctrl + c keeping doing...
+        while not rospy.is_shutdown():
+	        # publish the velocity
+            self.cmd_vel.publish(move_cmd)
+	        # wait for 0.1 seconds (10 HZ) and publish again
+            r.sleep()
+
+            if (gesture != 0):
+                # There is a gesture so call the function.
+                if (gesture == 1 or gesture == 2):
+                    self.snaking()
+                
+                # Add other gestures
+            else:
+                # No gesture, so normal motion.
+                move_cmd.linear.x = (max_distance - left_distance) * scalar 
+                # let's turn at 0 radians/s
+                move_cmd.angular.z = (max_distance - right_distance) * scalar 
+                        
+        
+    def shutdown(self):
+        # stop turtlebot
+        rospy.loginfo("Stop TurtleBot")
+	    # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
+        self.cmd_vel.publish(Twist())
+	   # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
+        rospy.sleep(1)
+
+    def snaking(self):
+        # Snaking back and forth. Just some fixed value.
+        move_cmd.linear.x = 1
+        angular_freq = 0
+        step = 0.1
+
+        if gesture == 2:
+            # Start snaking other way
+            step = -0.1
+            while True:
+                # Basically if we start rotating too far, then make
+                # it start rotating back the other way.
+                if angular_freq >= 3 or angular_freq <= -3:
+                    step *= -1
+                elif angular_freq <= -3:
+                    # One snake each way is enough.
+                    break
+                move_cmd.angular.z += step
+
+        # Reset back to no gesture after doing full snake.
+        gesture = 0
+
+        # Then will just return back to init.
+
+ 
+if __name__ == '__main__':
+    try:
+        GoForward()
+    except:
+        rospy.loginfo("GoForward node terminated.")
+
+
+# MQTT thread that receives, decodes data and then sets global flag for 
+# the ROS. Set gesture, left and right distance values based on that.
