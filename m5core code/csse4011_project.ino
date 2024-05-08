76147976acb64ab819573e57d728dc0a82dc7d68
Henry <h.sommerville@student.uq.edu.au>
2024-05-08 20:22:16 +1000

    Uart working between M5Core and nucleo

--- m5core code/csse4011_project.ino
+++ m5core code/csse4011_project.ino
@@ -1,14 +1,10 @@
-/*
-*******************************************************************************
-* Copyright (c) 2021 by M5Stack
-*                  Equipped with M5Core2 sample source code
-*                                M5Core2
-* Visit for more information: https://docs.m5stack.com/en/core/core2
-* : https://docs.m5stack.com/zh_CN/core/core2
-*
-* Describe: MQTT.
-* Date: 2021/11/5
-*******************************************************************************
+/**
+********************************************************************************
+* @author Henry Sommerville - s4702018
+* @date 08.05.2024
+* @brief M5 Core sketch for final project
+********************************************************************************
+*************************************************************************
 */
 #include "M5Core2.h"
 #include <WiFi.h>
@@ -24,148 +20,27 @@ PubSubClient client(espClient);
 
 #define PACKET_START 0xAA
 #define MODE_DEFAULT 0x01
-#define MODE_DESTURE 0x02
+#define MODE_GESTURE 0x02
 
-// Configure the name and password of the connected wifi and your MQTT Serve
-// host.
-const char* ssid        = "infrastructure";
-const char* password    = "3wbq4fAm4uij";
-const char* mqtt_server = "csse4011-iot.zones.eait.uq.edu.au";
+#define MAX_DATA_LENGTH 60 // Maximum length of data packet, adjust as needed
 
-const int trigPin = 33;
-const int echoPin = 32;
 
-long duration;
-float distance;
+int x_pos = 110;
+int y_pos = 110;
+int old_x_pos = 110;
+int old_y_pos = 110;
 
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE (50)
-char msg[MSG_BUFFER_SIZE];
-int value = 0;
+uint8_t data[MAX_DATA_LENGTH];
 
-void setupWifi();
-void callback(char* topic, byte* payload, unsigned int length);
-void reConnect();
-void setGrid();
-void drawTurtleBot(uint32_t x, uint32_t y);
 
-uint16_t x = 0;
-uint16_t y = 0;
-
-uint16_t left;
-uint16_t right;
-
-uint32_t wait;
-
-void setup() {
-    M5.begin();
-    wait = 0;
-    setupWifi();
-    client.setServer(mqtt_server, 1883);  // Sets the server details.
-    client.setCallback(callback);  // Sets the message callback function.
+void setupWifi(const char* ssid, const char* password) {
     delay(10);
-    M5.Lcd.fillScreen(BLACK);
-    delay(10);
-    setGrid();
-    M5.Lcd.setTextColor(WHITE);
-    M5.Lcd.setTextSize(1);
-    Serial2.begin(115200, SERIAL_8N1, 13, 14);
-}
-
-void loop() { 
-    if (!client.connected()) {
-      reConnect();
-    }
-
-    client.loop();
-    // Drawing Bounding Box
-    // Top line
-
-    if (Serial2.available()) {
-      int start = Serial2.read();
-      int command = Serial2.read();
-      int length = Serial2.read();
-      uint16_t data[length];
-      for (int i = 0; i < length; i++) {
-        data[i] = Serial2.read();
-      }
-      left = (data[0] << 8) | data[1];
-      right = (data[2] << 8) | data[3];
-      //M5.Lcd.setCursor(220, 70);
-      //M5.Lcd.printf("Serial2:%d\n", ch);
-      //delay(10);
-    }
-
-    M5.Lcd.setCursor(220, 10);
-    M5.Lcd.printf("Gesture:");
-    delay(10);
-
-    M5.Lcd.setCursor(220, 40);
-    M5.Lcd.printf("Coords: (%d, %d)", x, y);
-    delay(10);
-
-    if (left != 0) {
-      drawTurtleBot(left, right);
-    } else {
-      drawTurtleBot(CENTRE_X, CENTRE_Y);
-    }
-}
-
-void setGrid() {
-  // Draw Grid bounding box
-  // Top line
-    M5.Lcd.drawFastHLine(10, 10, 200, YELLOW);
-    delay(10);
-    // Bottom line
-    M5.Lcd.drawFastHLine(10, 210, 200, YELLOW);
-    delay(10);
-    // Left vertical
-    M5.Lcd.drawFastVLine(10, 10, 200, YELLOW);
-    delay(10);
-    // Right vertical
-    M5.Lcd.drawFastVLine(210, 10, 200, YELLOW);
-    delay(10);
-
-    // Drawing Grid lines
-    // Vertical Grid lines
-    M5.Lcd.drawFastVLine(60, 10, 200, YELLOW);
-    delay(10);   
-
-    M5.Lcd.drawFastVLine(110, 10, 200, YELLOW);
-    delay(10); 
-
-    M5.Lcd.drawFastVLine(160, 10, 200, YELLOW);
-    delay(10); 
-
-    // Horizontal grid lines
-    M5.Lcd.drawFastHLine(10, 60, 200, YELLOW);
-    delay(10);
-
-    M5.Lcd.drawFastHLine(10, 110, 200, YELLOW);
-    delay(10);
-
-    M5.Lcd.drawFastHLine(10, 160, 200, YELLOW);
-    delay(10);
-}
-
-void drawTurtleBot(uint32_t x, uint32_t y) {
-  // Draw TurtleBot centred at x and y
-  M5.Lcd.fillRect(x - PADDING, y - PADDING, 2 * PADDING, 2 * PADDING, BLUE);
-  delay(10);
-}
-
-void setupWifi() {
-    delay(10);
-    //M5.Lcd.printf("Connecting to %s", ssid);
-    WiFi.mode(
-        WIFI_STA);  // Set the mode to WiFi station mode. 
+    WiFi.mode(WIFI_STA);  // Set the mode to WiFi station mode. 
     WiFi.begin(ssid, password);  // Start Wifi connection. 
 
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
-        //M5.Lcd.print(".");
     }
-    //M5.Lcd.printf("\nSuccess\n");
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -193,4 +68,224 @@ void reConnect() {
             delay(5000);
         }
     }
+}
+
+void MQTT_init(const char* mqtt_server, const char* ssid, const char* password) {
+    setupWifi(ssid, password);
+    client.setServer(mqtt_server, 1883);  // Sets the server details.
+    client.setCallback(callback);  // Sets the message callback function.
+}
+
+void draw_grid() {
+  // Draw Grid bounding box
+  // Top line
+    M5.Lcd.drawFastHLine(10, 10, 200, YELLOW);
+    delay(10);
+    // Bottom line
+    M5.Lcd.drawFastHLine(10, 210, 200, YELLOW);
+    delay(10);
+    // Left vertical
+    M5.Lcd.drawFastVLine(10, 10, 200, YELLOW);
+    delay(10);
+    // Right vertical
+    M5.Lcd.drawFastVLine(210, 10, 200, YELLOW);
+    delay(10);
+
+    // Drawing Grid lines
+    // Vertical Grid lines
+    for (int c = 0; c < 3; c++) {
+      M5.Lcd.drawFastVLine(60 + (c * 50), 10, 200, YELLOW);
+      delay(10);
+    }
+
+    //Horizontal Lines
+    for (int r = 0; r < 3; r++) {
+      M5.Lcd.drawFastHLine(10, 60 + (r * 50), 200, YELLOW);
+      delay(10);
+    }
+
+}
+
+void drawTurtleBot(uint32_t x, uint32_t y) {
+  // Draw TurtleBot centred at x and y
+  M5.Lcd.fillRect(x - PADDING, y - PADDING, 2 * PADDING, 2 * PADDING, BLUE);
+  delay(10);
+}
+
+void lcd_draw_coordinates(int x, int y) {
+  // Create black rectangle over old coordinates
+  M5.Lcd.fillRect(220, 40, 180, 20, BLACK);
+  M5.Lcd.setCursor(220, 40);
+  M5.Lcd.printf("Coords: (%d, %d)", x, y);
+  delay(10);
+}
+
+void lcd_handler() {
+  M5.Lcd.setCursor(220, 10);
+  M5.Lcd.printf("Gesture:");
+  delay(10);
+
+  if (old_x_pos != x_pos || old_y_pos != y_pos) {
+    old_x_pos = x_pos;
+    old_y_pos = y_pos;
+    lcd_draw_coordinates(x_pos, y_pos);
+
+    drawTurtleBot(x_pos, y_pos);
+
+  }
+}
+
+void LCD_init() {
+  // Draw initial Grid
+    delay(10);
+    M5.Lcd.fillScreen(BLACK);
+    delay(10);
+    draw_grid();
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.setTextSize(1);
+    M5.Lcd.setCursor(220, 40);
+}
+
+void UART_init() {
+  Serial2.begin(115200, SERIAL_8N1, 13, 14);
+}
+
+void handle_gesture(uint8_t gesture) {
+    const char* mqttTopic = "URANUS-CRIMSON_M5CoreUpload";
+    
+    StaticJsonDocument<200> doc;
+    doc["timestamp"] = timeStamp;
+    doc["gesture"] = gesture;
+    doc["left"] = 0;
+    doc["right"] = 0;
+
+    String jsonString;
+    serializeJson(doc, jsonString);
+    
+
+    if (client.publish(mqttTopic, jsonString.c_str())) {
+      // idk if this does anything
+        Serial.println("Published to MQTT");
+    } else {
+        Serial.println("Failed to publish to MQTT");
+    }
+}
+
+void handle_packet(uint8_t* data) {
+  int command = data[0];
+  int length = data[1];
+  if (command == MODE_DEFAULT && length == 4) {
+    // handle ultrasonic measurements
+    uint16_t left = ((uint16_t)data[2] << 8) | (uint16_t)data[3];
+    uint16_t right = ((uint16_t)data[4] << 8) | (uint16_t)data[5];
+
+    // Call handle function
+    handle_ultrasonic_data(left, right);
+  } else if (command == MODE_GESTURE) {
+    // handle gesture
+    gesture = data[3];
+    handle_gesture(gesture);
+  } else {
+    return;
+  }
+}
+
+void handle_ultrasonic_data(uint16_t left, uint16_t right) {
+  // publish to mqtt
+
+    const char* mqttTopic = "URANUS-CRIMSON_M5CoreUpload";
+    
+    StaticJsonDocument<200> doc;
+    doc["timestamp"] = timeStamp;
+    doc["gesture"] = 0;
+    doc["left"] = left;
+    doc["right"] = right;
+
+    String jsonString;
+    serializeJson(doc, jsonString);
+    
+
+    if (client.publish(mqttTopic, jsonString.c_str())) {
+      // idk if this does anything
+        Serial.println("Published to MQTT");
+    } else {
+        Serial.println("Failed to publish to MQTT");
+    }
+}
+
+int uart_poll() {
+  int start = 0;
+  int length = 0;
+  if (Serial2.available()) {
+      start = Serial2.read();
+      if (start == PACKET_START) {
+        while (!Serial2.available()) {}
+        data[0] = Serial2.read();
+        while (!Serial2.available()) {}
+        length = Serial2.read();
+        data[1] = length;
+        if (length != 0 && length <= MAX_DATA_LENGTH) {
+          for (int i = 2; i < length + 2; i++) {
+            while (!Serial2.available()) {}
+              data[i] = Serial2.read();
+            }
+            return 1;
+        } else {
+          return 0;
+        }
+      } else {
+        return 0 ;
+      }
+      return 0;
+      
+    }
+    return 0;
+}
+
+
+
+void setup() {
+
+  const char* ssid = "infrastructure";
+  const char* password = "a3s7fSbjs4qS";
+  const char* mqtt_server = "csse4011-iot.zones.eait.uq.edu.au";
+
+ 
+
+
+  // Initialise M5 Core
+    M5.begin();
+
+    // Setup MQTT Communication
+    MQTT_init(mqtt_server, ssid, password);
+    
+    // Initialise Serial Communication
+    UART_init();
+
+    //init LCD
+    LCD_init();
+
+
+    // Start infinite loop I have done this to avoid globals
+    while (1) {
+      Check if client has lost connection, if so reconnect
+      if (!client.connected()) {
+        reConnect();
+      }
+
+      // Run Client Loop
+      client.loop();
+
+
+      // Check Serial Communication
+      if (uart_poll() == 1) {
+        handle_packet(data);
+      }
+
+      lcd_handler();
+    }
+}
+
+void loop() {
+
 }
--- mycode/apps/FP/src/main.c
+++ mycode/apps/FP/src/main.c
@@ -13,13 +13,14 @@
 #include <zephyr/drivers/gpio.h>
 #include <limits.h>
 
+#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define PACKET_START 0xAA
-#define MODE_DEFAULT 0x00
-#define MODE_GESTURE 0x01
+#define MODE_DEFAULT 0x01
+#define MODE_GESTURE 0x02
 
 #define ZEPHYR_USER_NODE DT_PATH(zephyr_user)
 
@@ -69,9 +70,9 @@ uint32_t get_distance(const struct gpio_dt_spec* trig, const struct gpio_dt_spec
 
         stop_time = k_cycle_get_32();
         cycles_spent = stop_time - start_time;
-        // if (cycles_spent > 1266720) {
-        //     return 0;
-        // }
+        if (cycles_spent > 1266720) {
+            return 0;
+        }
         ms_duration = k_cyc_to_us_floor32(cycles_spent);
         cm = ms_duration * 0.034 / 2;
 
@@ -106,20 +107,191 @@ int main(void) {
     uint32_t right_distance;
     uint8_t mode = MODE_DEFAULT;
     uint8_t packet[7];
+    time_t first_gesture_det_times;
+    time_t second_gesture_det_times;
+    uint8_t first_gesture_detected;
+    uint8_t second_gesture_detected;
 
-
+    mode = MODE_GESTURE;
     while(1) {
        right_distance = get_distance(&trig_right, &echo_right);
        printk("RIGHT DISTANCE: %d\n", right_distance);
-       k_sleep(K_MSEC(500));
        left_distance = get_distance(&trig_left, &echo_left);
        printk("LEFT DISTANCE: %d\n", left_distance);
-       k_sleep(K_MSEC(500));
 
        //Get distance for both ultrasonic.
+       if (1) {
+            mode = MODE_GESTURE;
+       } else {
+            mode = MODE_DEFAULT; 
+       }
+        mode = MODE_DEFAULT;
 
         if (mode == MODE_GESTURE) {
-            //Check if there is a gesture? not exactly sure how to do this
+
+            //TODO Check if the opposite sensor is zero, or greater than a certain value.
+            
+            // ############################### GESTURE 1 #########################//
+            // This is the right, left, right gesture.
+            if (right_distance >= 5 && right_distance <= 50) {
+                first_gesture_det_times = k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000;
+
+                // Note that we have received our first half of the gesture
+                // and then "scan" for the second half of the gesture for 5s.
+                while((k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000 - first_gesture_det_times) <= 5) {
+                    if (left_distance >= 5 && left_distance <= 50) {
+                        second_gesture_det_times = k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000;
+                        printk("second");
+
+                        // Note that we have received our second part of the gesture
+                        // and then "scan" for the final part of the gesture for 5s.
+                        second_gesture_detected = 1;
+                        // Also want to break out of inner while loop.
+                        break;
+                    }
+                }
+
+                if (second_gesture_detected) {
+                    // Looking for final part of the gesture. 
+                    while((k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000 - second_gesture_det_times) <= 5) {
+                        if (right_distance >= 5 && right_distance <= 50) {
+                            printk("left right left\n");
+                            // SET DISTANCES TO BE SENT VIA UART. Snake right to left.
+                            packet[0] = PACKET_START;
+                            packet[1] = MODE_GESTURE;
+                            packet[2] = 1;
+                            packet[3] = 0x01;
+                            s4702018_uart_transmit(packet, 4);
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+            // ############################### GESTURE 2 #########################//
+            // This is the left, right, left gesture.
+            if (left_distance >= 45 && left_distance <= 55) {
+                first_gesture_det_times = k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000;
+
+                // Note that we have received our first half of the gesture
+                // and then "scan" for the second half of the gesture for 5s.
+                while((k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000 - first_gesture_det_times) <= 5) {
+                    if (right_distance >= 45 && right_distance <= 55) {
+                        second_gesture_det_times = k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000;
+
+                        // Note that we have received our second part of the gesture
+                        // and then "scan" for the final part of the gesture for 5s.
+                        second_gesture_detected = 1;
+                        // Also want to break out of inner while loop.
+                        break;
+                    }
+                }
+
+                if (second_gesture_detected) {
+                    // Looking for final part of the gesture. If this doesn't
+                    // happen then we have only detected the first part of the
+                    // gesture and should drop out.
+                    while((k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000 - second_gesture_det_times) <= 5) {
+                        if (left_distance >= 45 && left_distance <= 55) {
+                            printk("right left right");
+                            // SET DISTANCES TO BE SENT VIA UART. Snake left to right.
+                            packet[0] = PACKET_START;
+                            packet[1] = MODE_GESTURE;
+                            packet[2] = 1;
+                            packet[3] = 0x02;
+                            s4702018_uart_transmit(packet, 4);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // ############################### GESTURE 3 #########################//
+            // This is the right-side down, up, down gesture. 
+            // TO ACTIVATE: Move hand from (10, 30)cm to (40, 60)cm to (10, 30)cm within
+            //              a couple of seconds ON THE RIGHT SENSOR.
+            if (right_distance >= 10 && right_distance <= 30) {
+                first_gesture_det_times = k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000;
+
+                // Note that we have received our first half of the gesture
+                // and then "scan" for the second half of the gesture for 5s.
+                while((k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000 - first_gesture_det_times) <= 5) {
+                    if (right_distance >= 40 && right_distance <= 60) {
+                        second_gesture_det_times = k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000;
+
+                        // Note that we have received our second part of the gesture
+                        // and then "scan" for the final part of the gesture for 5s.
+                        second_gesture_detected = 1;
+                        // Also want to break out of inner while loop.
+                        break;
+                    }
+                }
+
+                if (second_gesture_detected) {
+                    // Looking for final part of the gesture. If this doesn't
+                    // happen then we have only detected the first part of the
+                    // gesture and should drop out.
+                    while((k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000 - second_gesture_det_times) <= 5) {
+                        if (right_distance >= 10 && right_distance <= 30) {
+                            printk("right side up down");
+                            // SET DISTANCES TO BE SENT VIA UART. Rotate left.
+                            packet[0] = PACKET_START;
+                            packet[1] = MODE_GESTURE;
+                            packet[2] = 1;
+                            packet[3] = 0x03;
+                            s4702018_uart_transmit(packet, 4);
+                            break;
+                        }
+                    }
+                }  
+            }
+
+
+
+            // ############################### GESTURE 4 #########################//
+            // This is the left-side down, up, down gesture. 
+            // TO ACTIVATE: Move hand from (10, 30)cm to (40, 60)cm to (10, 30)cm within
+            //              a couple of seconds ON THE LEFT SENSOR.
+            if (left_distance >= 10 && left_distance <= 30) {
+                first_gesture_det_times = k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000;
+
+                // Note that we have received our first half of the gesture
+                // and then "scan" for the second half of the gesture for 5s.
+                while((k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000 - first_gesture_det_times) <= 5) {
+                    if (left_distance >= 40 && left_distance <= 60) {
+                        second_gesture_det_times = k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000;
+
+                        // Note that we have received our second part of the gesture
+                        // and then "scan" for the final part of the gesture for 5s.
+                        second_gesture_detected = 1;
+                        // Also want to break out of inner while loop.
+                        break;
+                    }
+                }
+
+                if (second_gesture_detected) {
+                    // Looking for final part of the gesture. If this doesn't
+                    // happen then we have only detected the first part of the
+                    // gesture and should drop out.
+                    while((k_cyc_to_ms_floor32(k_cycle_get_32()) * 1000 - second_gesture_det_times) <= 5) {
+                        if (left_distance >= 10 && left_distance <= 30) {
+                            printk("left side up down detected");
+                            
+                            // SET DISTANCES TO BE SENT VIA UART. Rotate left.
+                            packet[0] = PACKET_START;
+                            packet[1] = MODE_GESTURE;
+                            packet[2] = 1;
+                            packet[3] = 0x04;
+                            s4702018_uart_transmit(packet, 4);
+                            break;
+                        }
+                    }
+                }  
+            }
+
+            second_gesture_detected = 0;
+
         } else {
             // Send distances via UART
             packet[0] = PACKET_START;
@@ -130,6 +302,7 @@ int main(void) {
             packet[5] = (right_distance >> 8) & 0xFF;
             packet[6] = (right_distance) & 0xFF;
             s4702018_uart_transmit(packet, 7);
+            k_sleep(K_MSEC(500));
         }
 
 
