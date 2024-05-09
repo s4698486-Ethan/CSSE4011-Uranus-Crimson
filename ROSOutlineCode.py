b0218340ed2549042bbee483399eb9b3fe7ecae4
Mark Kandathiparambil <m.kandathiparambil@uqconnect.edu.au>
2024-05-10 09:58:57 +1000

    add rclpy spin

--- ROSOutlineCode.py
+++ ROSOutlineCode.py
@@ -80,8 +80,14 @@ class MoveTurtle(Node):
 
 def main (args=None):
     rclpy.init(args=args)
-    MoveTurtle()
-
+    publisher = MoveTurtle()
+    rclpy.spin(publisher)
+    
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    publisher.destroy_node()
+    rclpy.shutdown()
  
 if __name__ == '__main__':
     main()
