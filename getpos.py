35b04aa974473603672371280e7d7df3bbaf8ff7
Mark K <mfkandathiparambil@gmail.com>
2024-05-15 13:20:17 +1000

    Add amcl position reading and initial pose publishing

--- getpos.py
+++ getpos.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry
 
 class Pos_Sub(Node):
@@ -9,13 +10,39 @@ class Pos_Sub(Node):
         
 	    # Create a publisher which can "talk" to TurtleBot and tell it to move
         # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
-        self.subscription = self.create_subscription(Odometry, '/odom', self.cb, 10)
-    def cb(self, msg):
-        # Extract x, y coordinates from the transform message
-        x = msg.pose.pose.position.x
-        y = msg.pose.pose.position.y
-        print("TurtleBot3 coordinates - x: {}, y: {}".format(x, y))
+        self.odom_x = 0
+        self.odom_y = 0
+        self.odom_sub = self.create_subscription(Odometry, '/odom', self.cb, 10)
+        self.amcl_sub = self.create_subscription(PoseWithCovarianceStamped, '/amcl_pose', self.cb_2, 10)
+        self.init_pose_pub = self.create_publisher(PoseWithCovarianceStamped, '/initialpose', 10)
+        self.init_pose = PoseWithCovarianceStamped()
+        self.init_pose.header.frame_id = 'map'
+        self.init_pose.pose.pose.position.x = 0.0
+        self.init_pose.pose.pose.position.y = 0.0
+        self.init_pose.pose.pose.position.z = 0.0
+        self.init_pose.pose.pose.orientation.z = -0.65
+        self.init_pose.pose.pose.orientation.w = 0.8
 
+        
+        timer_period = 0.1  # seconds
+        self.timer = self.create_timer(timer_period, self.node_callback)
+    def node_callback(self):
+        #print("pub")
+        #elf.init_pose_pub.publish(self.init_pose)
+        pass
+
+    def cb(self, msg):
+        self.timer = None
+        # Extract x, y coordinates from the transform message
+        self.odom_x = msg.pose.pose.position.x
+        self.odom_y = msg.pose.pose.position.y
+        
+    def cb_2(self, msg):
+        position = msg.pose.pose.position
+        x = position.x
+        y = position.y
+        print("odom coordinates - x: {}, y: {}".format(self.odom_x, self.odom_y))
+        print("AMCL POSE - x: {}, y: {}".format(x, y))
 def main(args=None):
     rclpy.init(args=args)
     subscriber = Pos_Sub()
