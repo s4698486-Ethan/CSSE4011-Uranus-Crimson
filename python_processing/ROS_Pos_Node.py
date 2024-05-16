046cb57eac8a7fd83e06a2a26a6b876f730a02c4
henrysommerville <henry.sommerville4@gmail.com>
2024-05-16 14:50:29 +1000

    SLAM localization working, with automatic launching :)

--- python_processing/ROS_Pos_Node.py
+++ python_processing/ROS_Pos_Node.py
@@ -11,12 +11,12 @@ class TurtlePos(Node):
         super().__init__('position_node')
         self.pos_mqtt = pos_mqtt
         self.mqtt_pos = mqtt_pos
-        self.odom_x = 0
-        self.odom_y = 0
+        
 	    # Create a publisher which can "talk" to TurtleBot and tell it to move
         # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
-        self.subscription = self.create_subscription(Odometry, '/odom', self.cb, 10)
-        self.amcl_sub = self.create_subscription(PoseWithCovarianceStamped, '/amcl_pose', self.cb_2, 10)
+        self.odom_sub = self.create_subscription(Odometry, '/odom', self.cb, 10)
+        #self.amcl_sub = self.create_subscription(PoseWithCovarianceStamped, '/amcl_pose', self.cb_2, 10)
+        self.SLAM_sub = self.create_subscription(PoseWithCovarianceStamped, '/pose', self.cb_2, 10)
 
     def cb(self, msg):
         # Extract x, y coordinates from the transform message
@@ -30,7 +30,7 @@ class TurtlePos(Node):
         x = position.x
         y = position.y
         print("odom coordinates - x: {}, y: {}".format(self.odom_x, self.odom_y))
-        print("AMCL POSE - x: {}, y: {}".format(x, y))
+        print("SLAM POSE - x: {}, y: {}".format(x, y))
         self.pos_mqtt.put([packet.MODE_POSITION, x, y])
 
 
