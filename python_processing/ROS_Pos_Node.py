ea91b95e17cd285e5bfee04912e67258f9ecaaf8
Mark K <mfkandathiparambil@gmail.com>
2024-05-16 16:09:26 +1000

    add kalman filtering to odom and slam positions - UNTESTED

--- python_processing/ROS_Pos_Node.py
+++ python_processing/ROS_Pos_Node.py
@@ -4,6 +4,8 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry
 import packet as packet
 import time
+import numpy as np
+from Kalmin import Kalman
 
 
 class TurtlePos(Node):
@@ -13,25 +15,38 @@ class TurtlePos(Node):
         self.mqtt_pos = mqtt_pos
         self.odom_x = 0
         self.odom_y = 0
+        self.filtered_position = 0
 	    # Create a publisher which can "talk" to TurtleBot and tell it to move
         # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
         self.subscription = self.create_subscription(Odometry, '/odom', self.cb, 10)
         self.amcl_sub = self.create_subscription(PoseWithCovarianceStamped, '/amcl_pose', self.cb_2, 10)
 
+        # Initialize Kalman filter
+        x_init = np.array([0, 0, 0, 0])  # Initial state estimate (changed to (0,0))
+        cov_init = np.eye(4) * 0.1  # Initial covariance matrix
+        meas_err_slam = 0.025 # Measurement error for ultrasound
+        meas_err_odom = 0.5  # Measurement error for RF
+        proc_err = 1e-6  # Process error
+        self.kalman_filter = Kalman(x_init, cov_init, meas_err_slam, meas_err_odom, proc_err)
+
     def cb(self, msg):
         # Extract x, y coordinates from the transform message
         self.odom_x = msg.pose.pose.position.x
         self.odom_y = msg.pose.pose.position.y
-
+        self.kalman_filter.update_odom((self.odom_x, self.odom_y))
+        self.filtered_position = self.kalman_filter.x_hat
         
         
     def cb_2(self, msg):
         position = msg.pose.pose.position
         x = position.x
         y = position.y
-        print("odom coordinates - x: {}, y: {}".format(self.odom_x, self.odom_y))
-        print("AMCL POSE - x: {}, y: {}".format(x, y))
-        self.pos_mqtt.put([packet.MODE_POSITION, x, y])
+        self.kalman_filter.update_slam((x, y))
+        self.filtered_position = self.kalman_filter.x_hat
+        # print("odom coordinates - x: {}, y: {}".format(self.odom_x, self.odom_y))
+        # print("AMCL POSE - x: {}, y: {}".format(x, y))
+        # self.pos_mqtt.put([packet.MODE_POSITION, x, y])
+        self.pos_mqtt.put([packet.MODE_POSITION, self.filtered_position[0], self.filtered_position[1]])
 
 
 
