6022e4b5ff57acb16c66e74eb6fb70fe86f5f1a8
henrysommerville <henry.sommerville4@gmail.com>
2024-05-16 11:45:53 +1000

    position using amcl

--- python_processing/ROS_Pos_Node.py
+++ python_processing/ROS_Pos_Node.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry
 import packet as packet
 import time
@@ -11,20 +11,27 @@ class TurtlePos(Node):
         super().__init__('position_node')
         self.pos_mqtt = pos_mqtt
         self.mqtt_pos = mqtt_pos
-        
+        self.odom_x = 0
+        self.odom_y = 0
 	    # Create a publisher which can "talk" to TurtleBot and tell it to move
         # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
         self.subscription = self.create_subscription(Odometry, '/odom', self.cb, 10)
+        self.amcl_sub = self.create_subscription(PoseWithCovarianceStamped, '/amcl_pose', self.cb_2, 10)
 
     def cb(self, msg):
         # Extract x, y coordinates from the transform message
-        x = msg.pose.pose.position.x
-        y = msg.pose.pose.position.y
-        print("TurtleBot3 coordinates - x: {}, y: {}".format(x, y))
+        self.odom_x = msg.pose.pose.position.x
+        self.odom_y = msg.pose.pose.position.y
 
-        self.pos_mqtt.put([packet.MODE_POSITION, x, y])
         
-
+        
+    def cb_2(self, msg):
+        position = msg.pose.pose.position
+        x = position.x
+        y = position.y
+        print("odom coordinates - x: {}, y: {}".format(self.odom_x, self.odom_y))
+        print("AMCL POSE - x: {}, y: {}".format(x, y))
+        self.pos_mqtt.put([packet.MODE_POSITION, x, y])
 
 
 
